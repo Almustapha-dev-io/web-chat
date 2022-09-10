144d4898617ba91664c96c36d602bc218bda96c0
Almustapha Ibrahim <almustaphamuha98@gmail.com>
2022-09-10 23:58:37 +0100

    Add username on chat ui

--- src/Components/Chat.tsx
+++ src/Components/Chat.tsx
@@ -1,10 +1,11 @@
-import { Button, Flex, HStack } from '@chakra-ui/react';
+import { Button, Flex, HStack, Text } from '@chakra-ui/react';
 import { useAppSelector } from 'hooks';
 import ChatInput from './ChatInput';
 import ChatList from './ChatList';
 
 const Chat = () => {
 	const { messages } = useAppSelector((s) => s.messaging);
+	const { user } = useAppSelector((s) => s.user);
 
 	return (
 		<Flex
@@ -14,7 +15,8 @@ const Chat = () => {
 			mx="auto"
 			flexDirection="column"
 		>
-			<HStack w="full" py={2} justify="flex-end">
+			<HStack w="full" py={2} justify="space-between">
+				<Text fontSize="sm">{user?.username}</Text>
 				<Button size="sm">Sign out</Button>
 			</HStack>
 			<ChatList messages={messages} />

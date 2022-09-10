01c3bd8c46748e54eafc91f5b7eda11ae32dd7d7
Almustapha Ibrahim <almustaphamuha98@gmail.com>
2022-09-10 22:14:54 +0100

    Setup messaging redux slice

---
+++ src/feature/messaging-slice.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { IMessage } from 'interfaces/messaging';
+
+type MessagingState = {
+	messages: IMessage[];
+};
+
+const initialState: MessagingState = {
+	messages: [],
+};
+
+const slice = createSlice({
+	name: 'messaging',
+	initialState,
+	reducers: {
+		sendMessage(state, { payload }: PayloadAction<IMessage>) {
+			state.messages.push(payload);
+		},
+	},
+});
+
+export const { sendMessage } = slice.actions;
+export default slice.reducer;

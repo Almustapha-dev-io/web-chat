ce2b210117b5422fbf25dece4fd99e7cb0f7518a
Almustapha Ibrahim <almustaphamuha98@gmail.com>
2022-09-10 23:59:05 +0100

    Add authentication validation on App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { Box } from '@chakra-ui/react';
 import Chat from 'Components/Chat';
 import Signup from 'Components/Signup';
+import { useAppSelector } from 'hooks';
 
 const App = () => {
+	const isAuth = useAppSelector((s) => s.user.user !== null);
+
+	let content = <Signup />;
+	if (isAuth) {
+		content = <Chat />;
+	}
+
 	return (
 		<Box w="full" h="100vh" px={4} py={6} overflow="hidden">
-			{/* <Signup /> */}
-			<Chat />
+			{content}
 		</Box>
 	);
 };
